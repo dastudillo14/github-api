72822f7373339d90d0b12d7e5af13d2e65b00c9f
Dantee Astudillo <dantee-mauri@hotmail.com>
2025-09-07 22:35:21 -0500

    Add AppController to app.module.ts and update module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { GitHubModule } from './github/github.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [GitHubModule],
+  controllers:[AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
